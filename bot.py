f808ed9f91c567ddb902eb4111b2ee774d6918c1
Stark AI <119601417+Stark-AI-live@users.noreply.github.com>
2023-03-10 18:08:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,24 @@
 import asyncio
 from pyrogram import *
-from pyrogram.errors import *
 from pyrogram.handlers import *
 from pyrogram.types import *
+from pyrogram.errors import *
 import requests
 
-API_KEY = "YOUR_API_KEY"
-API_ID = "YOUR_APP_ID"
-API_HASH = "YOUR_APP_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+OWNER_API_KEY = "OWNER_API_KEY"
 OWNER_ID = "OWNER_ID"
+API_ID = "API_ID"
+API_HASH = "API_HASH"
+BOT_TOKEN = "BOT_TOKEN"
+ADMIN_API_KEY = "ADMIN_API_KEY"
 API_URL = "telegram.starkai.live"
 
 app = Client(name='STARK-API',api_id=API_ID,api_hash=API_HASH,bot_token=BOT_TOKEN)
 
+
 @app.on_message(filters.command(["start","help"]))
 async def start(_, message: Message):
-  return await message.reply("Welcome to Stark AntiSpam System!\n\nCommands!\n/start - Start The Bot\n/status - To get your Information\n/get_api - To generate API KEY (Admins)\n/check_api - Show your API KEY (Admins)\n/get_trust - To get the Users Trust Score\n/check - To check the status of the User\n/add_admin - To add Admins (Owner)\n/rm_admin - To remove Admins (owner)")
+  return await message.reply("Welcome to Stark AntiSpam System!\n\nCommands!\n/start - Start The Bot\n/status - To get your Information\n/admins - To Get the list of Admins\n/get_api - To generate API KEY (Admins)\n/check_api - Show your API KEY (Admins)\n/ban - To Ban any user! (Admins)\n/unban - To Unban any user! (Admins)\n/get_trust - To get the Users Trust Score\n/check - To check the status of the User\n/add_admin - To add Admins (Owner)\n/rm_admin - To remove Admins (owner)")
   
   
 @app.on_message(filters.command("add_admin"))
@@ -32,7 +34,7 @@ async def add_admin(_, message: Message):
       else:
         return await message.reply("Tag any user to make Admin!")
         
-      data = requests.get(f"https://{API_URL}/addsudo?user_id={user_id}&api_key={API_KEY}")
+      data = requests.get(f"https://{API_URL}/addsudo?user_id={user_id}&api_key={OWNER_API_KEY}")
       r = data.json()
       msg = r["message"]
       return await message.reply(msg)
@@ -49,7 +51,7 @@ async def rm_admin(_, message: Message):
       else:
         return await message.reply("Tag Admin to Remove!")
         
-      data = requests.get(f"https://{API_URL}/rmsudo?user_id={user_id}&api_key={API_KEY}")
+      data = requests.get(f"https://{API_URL}/rmsudo?user_id={user_id}&api_key={OWNER_API_KEY}")
       r = data.json()
       msg = r["message"]
       return await message.reply(msg)
@@ -96,28 +98,95 @@ async def get_trust(_, message: Message):
 
 @app.on_message(filters.command("status"))
 async def status(_, message: Message):
-    user_id = message.from_user.id
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        user_id = reply.from_user.id
+    else:
+        user_id = message.from_user.id
+        
     data = requests.get(f"https://{API_URL}/check_user?user_id={user_id}")
     r = data.json()
-    if user_id == OWNER_ID:
-        is_admin = "Onwer"
-    else:
-        is_admin = r['is_admin']
-        
+    is_admin = r['is_admin']       
+    spam_score = r['spam_score']
     is_banned = r['is_banned']
-    spam_score = r['spam_score'] * 100
-    return await message.reply(f"Your ID: `{user_id}`\nStatus: `{is_admin}`\nIs Banned: `{is_banned}`\nSpam Score: `{spam_score} %`")
-  
+    msg = f"User ID: `{user_id}`\nAdmin Status: `{is_admin}`\nIs Banned: `{is_banned}`\nSpam Score: `{spam_score} %`"
+    if is_banned == "True":
+      admin = r['admin']
+      reason = r['banned_reason']
+      time = r['banned_time']
+      msg += f"\nReason: `{reason}`\nAdmin: `{admin}`\nTime: `{time}`"
 
-@app.on_message(filters.text)
+    return await message.reply(msg)
+  
+@app.on_message(filters.command("admins"))
+async def admins(_, message: Message):
+  data = requests.get(f"https://{API_URL}/admins")
+  r = data.json()
+  admins = r['message']
+  return await message.reply(admins)
+
+@app.on_message(filters.command("ban"))
+async def ban(_, message: Message):
+      if len(message.command) < 2:
+         return await message.reply("Reason Not Provided")
+
+      reason = message.text.split(None, 1)[1]
+      if message.reply_to_message:
+        reply = message.reply_to_message
+        user_id = reply.from_user.id
+      else:
+        return await message.reply("Tag any user to Ban")
+        
+      data = requests.get(f"https://{API_URL}/ban_user?user_id={user_id}&api_key={ADMIN_API_KEY}&reason={reason}")
+      r = data.json()
+      msg = r["message"]
+      return await message.reply(msg)
+  
+@app.on_message(filters.command("unban"))
+async def unban(_, message: Message):
+      reason = message.text
+      if message.reply_to_message:
+        reply = message.reply_to_message
+        user_id = reply.from_user.id
+      else:
+        return await message.reply("Tag any user to Unban")
+        
+      data = requests.get(f"https://{API_URL}/unban_user?user_id={user_id}&api_key={ADMIN_API_KEY}")
+      r = data.json()
+      msg = r["message"]
+      return await message.reply(msg)
+
+@app.on_message(filters.incoming)
 async def check_message(client, message):
   msg = message.text
+  chat_id = message.chat.id
   user_id = message.from_user.id
   data = requests.get(f"https://{API_URL}/check_message?user_id={user_id}&message={msg}")
+  ban = requests.get(f"https://{API_URL}/check_user?user_id={user_id}")
   if data.status_code == 200:
     return
   else:
     print("System UnRecognised")
+   
+  rb = ban.json()
+  is_ban = rb['is_banned']
+  if is_ban == "True":
+    try:
+      await app.ban_chat_member(chat_id, user_id)
+      reason = rb['banned_reason']
+      admin = rb['admin']
+      time = rb['banned_time']
+      spam_score = rb['spam_score'] * 100
+      TEXT = f"User was banned in Stark AntiSpam System!\n\nUser ID: `{user_id}`\nReason: `{reason}`\nAdmin: `{admin}`\nSpam Score: `{spam_score} %`\nTime: `{time}`"
+      return await app.send_message(chat_id=chat_id,text=TEXT)
+    except Exception as e:
+      reason = rb['banned_reason']
+      admin = rb['admin']
+      time = rb['banned_time']
+      spam_score = rb['spam_score'] * 100
+      TEXT = f"User was banned in Stark AntiSpam System!\n\nUser ID: `{user_id}`\nReason: `{reason}`\nAdmin: `{admin}`\nSpam Score: `{spam_score} %`\nTime: `{time}`"
+      return await message.reply(TEXT)
+      
     
 
     
@@ -125,10 +194,11 @@ async def check_message(client, message):
 def main():
   try:
     app.run()
+    print("Started Successfully")
   except FloodWait as e:
     print(f"Sleeping for {e.value} seconds!")
 
 
 if __name__ == "__main__":
                    main()
-                   print("Started Successfully")
+                   
